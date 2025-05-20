d8c430ba8abd196ec86500028de559d977021001
mohsink <mohsinking998877@gmail.com>
2025-05-20 18:34:06 +0500

    Adding the data  Ing

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -19,6 +19,8 @@ class DataIngestion:
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+    
+    
         
         
     """ Read The Data """
@@ -28,20 +30,38 @@ class DataIngestion:
             collection_name = self.data_ingestion_config.collection_name
             self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
             collection = self.mongo_client[database_name][collection_name]
-            
             df = pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
                 df.drop(columns=["_id"])  
             df.replace({"na":np.nan}, inplace=True)  
-            
             return df  
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
+        
+        
+    def export_data_into_feature_store(self, dataframe:pd.DataFrame):
+        try:
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
+            # FOR CREATING FOLDER
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            dataframe.to_csv(feature_store_file_path,index=False, header=True)
+            return dataframe
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+   
+   
+   
         
     def initiate_data_ingestion(self):
         try:
             dataframe = self.export_data_as_dataframe()
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+        
+        
+    
             
